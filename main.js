6b9134ee69cc125fc41e282ceddcbf7ac9988e5c
S. Ben Stewart <sbenstewart@gmail.com>
2021-12-15 20:52:06 +0530

    code rewrite

--- main.js
+++ main.js
@@ -1,54 +1,59 @@
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
-
-var size = window.innerWidth / 4;
 var dpr = window.devicePixelRatio;
-canvas.width = dpr * window.innerWidth / 4;
-canvas.height = dpr * window.innerHeight / 4;
-context.scale(dpr, dpr);
-context.lineJoin = 'bevel';
+var width = dpr * window.innerWidth / 4;
+var height = dpr * window.innerHeight / 4;
 
-var line, dot,
-    odd = false, 
-    lines = [],
-    gap = (canvas.width + canvas.height) / 75;
-
-for(var y = gap / 2; y <= window.innerWidth / 4; y+= gap) {
-  odd = !odd;
-  line = [];
-  for(var x = gap / 4; x <= size; x+= gap) {
-    dot = {x: x + (odd ? gap/2 : 0), y: y};
-    line.push(dot);
-    context.beginPath();
-    context.arc(dot.x, dot.y, 1, 0, 2 * Math.PI, true);
-    context.fill();
+function sketch_idnameofdiv(p) {
+  p.setup = function () {
+    p.createCanvas(width, height);
+    p.clear();
   }
-  lines.push(line);
-}
 
-function drawTriangle(pointA, pointB, pointC) {
-  context.beginPath();
-  context.moveTo(pointA.x, pointA.y);
-  context.lineTo(pointB.x, pointB.y);
-  context.lineTo(pointC.x, pointC.y);
-  context.lineTo(pointA.x, pointA.y);
-  context.closePath();
-  context.stroke();
-}
+  p.draw = function () {
+    p.clear();
 
-var dotLine;
-odd = true;
+    var size = dpr * window.innerWidth / 4;
 
-for(var y = 0; y < lines.length - 1; y++) {
-  odd = !odd;
-  dotLine = [];
-  for(var i = 0; i < lines[y].length; i++) {
-    if(i + (y/2) < size/(gap*2) - 1) continue;
-    if(i - (y/2) >= size/(gap*2) + 1) continue;
-    dotLine.push(odd ? lines[y][i]   : lines[y+1][i]);
-    dotLine.push(odd ? lines[y+1][i] : lines[y][i]);
+    var line, dot,
+      odd = false,
+      lines = [],
+      gap = (width + height) / 75;
+
+    for (var y = dpr * gap / 2; y <= width; y += gap * dpr) {
+      odd = !odd;
+      line = [];
+      for (var x = dpr * gap / 4; x <= size; x += gap * dpr) {
+        dot = { x: x + (odd ? dpr * gap / 2 : 0), y: y };
+        line.push(dot);
+        p.fill(0);
+        p.noStroke();
+        p.circle(dot.x, dot.y, 3);
+      }
+      lines.push(line);
+    }
+
+
+    function drawTriangle(p, pointA, pointB, pointC) {
+      p.noFill();
+      p.stroke(0);
+      p.triangle(pointA.x, pointA.y, pointB.x, pointB.y, pointC.x, pointC.y)
+    }
+
+    var dotLine;
+    odd = true;
+
+    for (var y = 0; y < lines.length - 1; y++) {
+      odd = !odd;
+      dotLine = [];
+      for (var i = 0; i < lines[y].length; i++) {
+        if (i + (y / 2) < size / (dpr * gap * 2) - 1) continue;
+        if (i - (y / 2) >= size / (dpr * gap * 2) + 1) continue;
+        dotLine.push(odd ? lines[y][i] : lines[y + 1][i]);
+        dotLine.push(odd ? lines[y + 1][i] : lines[y][i]);
+      }
+      for (var i = 0; i < dotLine.length - 2; i++) {
+        drawTriangle(p, dotLine[i], dotLine[i + 1], dotLine[i + 2]);
+      }
+    }
   }
-  for(var i = 0; i < dotLine.length - 2; i++) {
-    drawTriangle(dotLine[i], dotLine[i+1], dotLine[i+2]);
-  }
 }
+new p5(sketch_idnameofdiv, 'canvas')
